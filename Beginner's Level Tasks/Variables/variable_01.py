f5d0a719d36c58352abe5a2f56951ac021a9e379
Iramshad06 <144264254+Iramshad06@users.noreply.github.com>
2025-01-13 13:01:36 +0530

    Internship Beginner Level Task-1

---
+++ Beginner's Level Tasks/Variables/variable_01.py
@@ -0,0 +1,4 @@
+#1. Create a variable named pi and store the value 22/7 in it. Now check the data type of this variable.
+pi = 22/7
+print("The value of pi is=", pi)
+print("The Type_of_Pi is:",type(pi))
