d11cc175ded03ffd3c243210b8b5a8bbe44d5840
Iramshad06 <144264254+Iramshad06@users.noreply.github.com>
2025-01-13 13:07:19 +0530

    Internship Beginner Level Task-1

--- Beginner's Level Tasks/Numbers/numbers_01.py
+++ Beginner's Level Tasks/Numbers/numbers_01.py
@@ -1,1 +1,7 @@
-
+#1. Write a function that takes two arguments, 145 and 'o', and uses the `format` function to return a formatted string. Print the result. Try to identify the representation used.
+def formatfunc(num, char):
+    val = "{},{}".format(num, char)
+    return val
+str = formatfunc(145, 'o')
+print("The Formatted String is :", str)
+print("The Representation used is:", type(str).__name__)
