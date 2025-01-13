e6a0a5b8c3093e09cf9ac3324e82b01f041926af
Iramshad06 <144264254+Iramshad06@users.noreply.github.com>
2025-01-13 13:03:32 +0530

    Internship Beginner Level Task-1

---
+++ Beginner's Level Tasks/Variables/variables_02.py
@@ -0,0 +1,4 @@
+#2. Create a variable called for and assign it a value 4. See whathappens and find out the reason behind the behavior that you see.
+for = 4
+print("The value of for is":, for)
+#The word "for" is a reserved keyword in python, which cannot be used as a name for any variables. Hence no output is produced and an error is generated
---
+++ Beginner's Level Tasks/Variables/variables_03.py
@@ -0,0 +1,6 @@
+#3. Store the principal amount, rate of interest, and time indifferent variables and then calculate the Simple Interest for 3 years. Formula: Simple Interest = P x R x T / 100
+Principal_amount=int(input("Enter the principal amount:"))
+Rate_of_interest= int(input("Enter the rate_of_interest:")) 
+Time_in_years=3
+SimpleInterest_SI=(Principal_amount*Rate_of_interest*Time_in_years)/100
+print(f"The Value of Simple Interest is = {SimpleInterest_SI}")
