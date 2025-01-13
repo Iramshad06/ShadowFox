2e80bddf3bcddac31b8e2038cceb113afed94b32
Iramshad06 <144264254+Iramshad06@users.noreply.github.com>
2025-01-13 13:12:13 +0530

    Internship Beginner Level Task-1

---
+++ Beginner's Level Tasks/Numbers/numbers_02.py
@@ -0,0 +1,10 @@
+#2. In a village, there is a circular pond with a radius of 84 meters. Calculate the area of the pond using the formula: Circle Area = πr^2. (Use the value 3.14 for π) 
+# Bonus Question: If there is exactly 1.4 liters of water in a square meter, what is the total amount ofwater in the pond? Print the answer without any decimal point init.
+r=84
+pi =3.14
+Area=pi*r*r
+print("The Area of circle is:",Area)
+#Pond has a Water amount of 1.4 litres in a square meter
+Amount_of_water=1.4
+Total_amount_of_water=(Area*Amount_of_water)
+print("The total amount of water contained in the pool is:", Total_amount_of_water)
